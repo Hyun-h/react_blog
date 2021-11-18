3599b18468c655214b444b62f562106e776e2e30
Hyun-h <hyeonahong@gmail.com>
2021-11-18 11:08:10 +0900

    useState를 써봅시다

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+/* eslint-disable */
+
 import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  //useState
   let [글제목, 글제목변경] = useState([
     "남자 코트 추천",
     "강남 우동 맛집",
     "옷가게 추천",
   ]);
+  let [like, likeUp] = useState(0);
+
+  function 제목바꾸기() {
+    글제목변경(["여자 코트 추천", "강남 우동 맛집", "옷가게 추천"]);
+  }
 
   return (
     <div className="App">
       <nav className="black-nav">
         <h1>개발 Blog</h1>
       </nav>
+      <button onClick={제목바꾸기}>클릭!</button>
       <section className="list_wrap">
         <article className="list_container">
           <div className="list">
-            <h2>{글제목[0]}</h2>
+            <h2>
+              {글제목[0]}
+              <span
+                onClick={() => {
+                  likeUp(like + 1);
+                }}
+              >
+                👍
+              </span>
+              {like}
+            </h2>
             <p>11월 17일</p>
             <hr />
           </div>
